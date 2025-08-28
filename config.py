c7244c24ffdbfbd6c252e0f1f11999464af659fb
Ivan Laptev <ivan.laptev952@gmail.com>
2025-08-28 16:22:52 +0300

    Mostly final version

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,3 +7,7 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = None
 CHECK_INTERVAL = 20
+CHECK_UPGRADES_INTERVAL = 600
+DATA_FILEPATH = Path("star_gifts.json")
+DATA_SAVER_DELAY = 60
+UPGRADES_CHAT_ID = None
